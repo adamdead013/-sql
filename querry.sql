3874bbfd6d886851a1fc544026805fa71329890d
adamdead013 <57434632+adamdead013@users.noreply.github.com>
2022-12-28 06:36:58 +0700

    Update querry.sql

--- querry.sql
+++ querry.sql
@@ -2,32 +2,27 @@ CREATE TABLE Items (
 	Id INT PRIMARY KEY,
 	"itemName" TEXT
 );
-
 INSERT INTO Items
 VALUES
 	(1, 'Something'),
 	(2, 'Something2'),
 	(3, 'Something3'),
-  (4, 'Something4'),
-  (5, 'Something5');
-
+  	(4, 'Something4'),
+	(5, 'Something5');
 CREATE TABLE Category (
 	Id INT PRIMARY KEY,
 	"categoryName" TEXT
 );
-
 INSERT INTO Categories
 VALUES
 	(1, 'SomeCategory'),
 	(2, 'SomeCategory2'),
 	(3, 'SomeCategory3');
-
 CREATE TABLE ItemsCategory (
 	ItemId INT FOREIGN KEY REFERENCES Items(Id),
 	CategoryId INT FOREIGN KEY REFERENCES Category(Id),
 	PRIMARY KEY (ItemId, CategoryId)
 );
-
 INSERT INTO ItemsCategory
 VALUES
 	(1, 1),
@@ -36,7 +31,6 @@ VALUES
   (3, 3),
   (4, 3),
   (5, 1);
-
 SELECT I."Name", C."Name"
 FROM Items I
 LEFT JOIN ItemsCategory IC
